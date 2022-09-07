3ac772de77808f1befd9257f0ebd996976c37722
OnezSadim <zenofrederik@gmail.com>
2022-09-07 14:07:15 +0200

    menu gemaakt

---
+++ js/navigatie.js
@@ -0,0 +1,17 @@
+/*
+alert('ben nu bezig met het menu')
+setInterval(function(){
+
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var widthNav = width - width * 0.5;
+  let gussy = document.getElementById("Nee");
+  gussy.style.width = widthNav * 0.25;
+  let sussyamogus = document.getElementsByClassName('navLink');
+  for (var i =0; i < sussyamogus.length; i++){
+    sussyamogus[i].style.width = widthNav * 0.25;
+  }
+}, 1000);
+alert(width)
+alert(widthNav)
+*/
