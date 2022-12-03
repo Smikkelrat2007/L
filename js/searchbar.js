1ceb86740506ffd996fe782accdd03a7645e04ea
OnezSadim <zenofrederik@gmail.com>
2022-12-04 00:02:24 +0100

    laatste commit

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,260 +1,121 @@
-console.log("het werkt!!");
+console.log("javascript searchbar.js is opgestart");
+let zoeken_op = "naam"
+
 const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
+const span = document.getElementById('span')
+
+
 let users = []
 const data = [
 {
   "id": 1,
-  "name": "boris is erg mooi en ik zou zn pik afzuigen als hij dat wou... maar ik geloof dat hij me nu haat omdat ik zn pik is een keer probeerde te zuien;()",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "https://fotoaanjemuur.nl/411/leidsegracht-amsterdam.jpg",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
+  "money": "40",
+  "name": "C&A chino beige",
+  "img1": "https://img01.ztat.net/article/spp-media-p1/666b6764fc444629b9cf028d2f713129/795d0b52dc4f4254964d848bda67c174.jpg",
+  "garment": "long_pants",
+  "color": "broun",
+  "size": "",
+  "gender": "man",
+  "type": "chino",
+  "material": "",
+  "rgb_colors": {
+    "color_rgb1": "rgb(148,109,41)"
   }
-}   ,
+},
 {
   "id": 2,
-  "name": "hoi",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "Bret",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
+  "money": "13",
+  "name": "Nike SportswearTrainingsbroek - midnight navy",
+  "img1": "https://img01.ztat.net/article/spp-media-p1/cd93c092a75b4bd4802542cc77fa3eb6/d9cbd18c5e3c44ea9d0ce2e06b05b45d.jpg?imwidth=1800",
+  "garment": "jeans",
+  "color": "donkerblauw",
+  "size": "",
+  "gender": "man",
+  "type": "oversized",
+  "material": "",
+  "rgb_colors": {
+    "color_rgb1": "rgb(0,0,100)"
   }
 },
 {
   "id": 3,
-  "name": "wowie",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "Bret",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
+  "money": "13",
+  "name": "Bershka wit met print groen",
+  "img1": "https://cdn.aboutstatic.com/file/images/9b1da765e6f9cdd1b1a46c83663e4f90.jpg?brightness=0.96&quality=75&trim=1&height=480&width=360",
+  "garment": "t-shirt",
+  "color": "broun",
+  "size": "",
+  "gender": "man",
+  "type": "oversized",
+  "material": "",
+  "rgb_colors": {
+    "color_rgb1": "rgb(255,255,255)"
   }
 },
 {
   "id": 4,
-  "name": "berta",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "Bret",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
+  "money": "30",
+  "name": "BershkaStraight leg jeans - light blue denim",
+  "img1": "https://img01.ztat.net/article/spp-media-p1/d04459c1e3af42859c371ef15953686b/cfd85f61ba184247915f17ea74810936.jpg?imwidth=1800",
+  "garment": "jeans",
+  "color": "lichtblauw",
+  "size": "",
+  "gender": "man",
+  "type": "oversized",
+  "material": "",
+  "rgb_colors": {
+    "color_rgb1": "rgb(100,100,255)"
   }
 },
 {
   "id": 5,
-  "name": "gert",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "Bret",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
+  "money": "60",
+  "name": "ConbipelBLEND - Trui - naturale",
+  "img1": "https://img01.ztat.net/article/spp-media-p1/a6a5f62fbca24411a3175c382380e570/a9405df3b4144ecb9c8bbab7ee3da546.jpg?imwidth=762",
+  "garment": "t-shirt",
+  "color": "donkerblauw",
+  "size": "",
+  "gender": "man",
+  "type": "oversized",
+  "material": "",
+  "rgb_colors": {
+    "color_rgb1": "rgb(0,0,100)"
   }
 },
 {
   "id": 6,
-  "name": "hihihiha",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "Bret",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
-  }
-},{
-  "id": 2,
-  "name": "boris zwietink zn hele mooie ...... wenkbrouwen",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "Bret",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
+  "money": "35",
+  "name": "Upscale by Mister TeeVIVE LA LIBERTE - T-shirt print - black acid washed",
+  "img1": "https://img01.ztat.net/article/spp-media-p1/c73c1ecf433b4c22bd3c3ef2b63730c9/90065d9569a647b6a4906d99fad86041.jpg?imwidth=762&filter=packshot",
+  "garment": "t-shirt",
+  "color": "zwart",
+  "size": "",
+  "gender": "man",
+  "type": "oversized",
+  "material": "",
+  "rgb_colors": {
+    "color_rgb1": "rgb(20,20,20)"
   }
 },
-{
-  "id": 3,
-  "name": "je bent echt heel erg mooi :)",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "Bret",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
-  }
-},
-{
-  "id": 4,
-  "name": "berta",
-  "img1": "https://media.indebuurt.nl/deventer/2020/08/15101850/foto-sander-claasen-1024x683.jpg",
-  "img2": "Bret",
-  "img3": "Bret",
-  "img4": "Bret",
-  "img5": "Bret",
-  "img6": "Bret",
-  "img7": "Bret",
-  "img8": "Bret",
-  "img9": "Bret",
-  "img10": "Bret",
-  "email": "",
-  "filters": {
-    "garment": "long_pants",
-    "color": "broun",
-    "size": "150",
-    "gender": "man",
-    "type": "cargo",
-    "price": "50",
-    "material": "flanel",
-    "rgb_colors": {
-      "color_rgb1": "rgb(148,109,41)"
-    }
-  }
-}
 ]
 
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
   users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
+    if (zoeken_op == "naam") {
+      const isVisible =
+        user.name.toLowerCase().includes(value)
+      user.element.classList.toggle("hide", !isVisible)
+    }
+    if (zoeken_op == "prijs") {
+      const isVisible =
+        user.money.toLowerCase().includes(value)
+      user.element.classList.toggle("hide", !isVisible)
+    } else {
+      console.log('error');
+    }
   })
 })
 
@@ -267,20 +128,30 @@ selectData = (data) => {
     const money = card.querySelector("[data-price]")
     const color_rgb = card.querySelector("[data-color_rgb]")
 
-    color_rgb.style.backgroundColor = user.filters.rgb_colors.color_rgb1;
+    color_rgb.style.backgroundColor = user.rgb_colors.color_rgb1;
     color_rgb.style.border = "solid 5px black";
     color_rgb.style.width = "30px";
     color_rgb.style.height = "30px";
 
-
+    header.style.fontFamilly = "'Montserrat', sans-serif";
 
 
 
     header.textContent = user.name
-    money.textContent = user.filters.price + "$"
+    money.textContent = user.money + "$"
     body.src = user.img1
     userCardContainer.append(card)
-    return { name: user.name, email: user.email, money: user.price, element: card }
+    return { name: user.name, email: user.email, money: user.money, element: card }
   })
 }
 selectData(data)
+
+function myFunction(a, b) {
+  if (zoeken_op == "prijs"){
+    zoeken_op = "naam";
+    span.innerHTML = "naam van het product ";
+  }else if (zoeken_op == "naam"){
+    zoeken_op = "prijs";
+    span.innerHTML = "prijs van het product";
+  }
+}
